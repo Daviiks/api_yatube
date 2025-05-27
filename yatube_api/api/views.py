a0064cf8449c12efa83ceb03af6300b3247666f5
David <45599491+Daviiks@users.noreply.github.com>
2025-05-27 22:30:45 +0600

    Update views.py

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from api.permissions import IsAuthorOrReadOnly
 from api.serializers import (CommentSerializer, GroupSerializer,
-PostSerializer, UserSerializer)
+                             PostSerializer, UserSerializer)
 from posts.models import Group, Post, User
 
 
